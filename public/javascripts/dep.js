8ce369e77a210ce2305c7066b6c8c40dd9fe2d36
Kim Hsiao <white.xtera@gmail.com>
2013-11-23 13:48:40 +0800

    inital njs_blog project with basic testing and environment setup

---
+++ public/javascripts/dep.js
@@ -0,0 +1,11 @@
+requirejs.config({
+	baseUrl: "javascripts",
+	path: {
+		jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js",
+		backbone: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js",
+		underscore: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js",
+		bootstrap: "bootstrap.min",
+		jqueryui: "//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"
+
+	}
+});
