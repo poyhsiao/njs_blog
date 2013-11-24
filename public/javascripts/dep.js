2e7ed273c686dd3cd6c963c343f671695b3a7ef0
Kim Hsiao <white.xtera@gmail.com>
2013-11-24 22:42:21 +0800

    1. update front end template as angular 2. fix conflict between angular and swig 3. include requirejs as front-end js dependence solver

--- public/javascripts/dep.js
+++ public/javascripts/dep.js
@@ -1,11 +1,44 @@
+/**
+ * for require js depends defination
+ */
 requirejs.config({
 	baseUrl: "javascripts",
-	path: {
-		jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js",
-		backbone: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js",
-		underscore: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js",
+	paths: {
+		jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min",
+		angular: "//ajax.googleapis.com/ajax/libs/angularjs/1.2.2/angular.min",
+		backbone: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min",
+		underscore: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min",
 		bootstrap: "bootstrap.min",
-		jqueryui: "//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"
+		jqueryui: "//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min",
+		main: "main"
+	},
+	shim: {
+		jquery: {
+			exports: '$'
+		},
+		angular: {
+			exports: 'angular'
+		},
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone'
+		},
+		bootstrap: {
+			deps: ['jquery']
+		},
+		jqueryui: {
+			deps: ['jquery']
+		},
+		main: {
+			deps: ['angular']
+		}
+	},
+	waitSeconds: 15
+});
 
-	}
+require(['main'], function() {
+	console.log("import main.js");
 });
